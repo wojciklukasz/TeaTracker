f1e288ea507c88489255b60a7dff3991de8ba406
Łukasz Wójcik <lukasz.wojcik.22@gmail.com>
2023-12-17 16:12:59 +0100

    Add 2 more missing on_deletes

--- tea/models.py
+++ tea/models.py
@@ -26,7 +26,7 @@ class Country(models.Model):
 
 class Province(models.Model):
     name = models.CharField(max_length=30)
-    country = models.ForeignKey(Country)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.name
@@ -34,7 +34,7 @@ class Province(models.Model):
 
 class Region(models.Model):
     name = models.CharField(max_length=30)
-    country = models.ForeignKey(Province)
+    country = models.ForeignKey(Province, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.name
