625232c31f38afc81014ffefb48480eac2c7cc7c
Łukasz Wójcik <lukasz.wojcik.22@gmail.com>
2023-12-22 23:38:42 +0100

    Add images and urls to items in lists

--- tea/urls.py
+++ tea/urls.py
@@ -1,7 +1,9 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
     path('', views.MainPageView.as_view(), name='main-page'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
