79d68efbd6359b83b70a50c1ca36dbc1b8069291
Łukasz Wójcik <lukasz.wojcik.22@gmail.com>
2024-02-04 19:39:49 +0100

    Create validations for harvest year and harvest date

--- tea/forms.py
+++ tea/forms.py
@@ -1,3 +1,6 @@
+from datetime import date
+from typing import Any
+
 from django import forms
 
 from .models import Tea
@@ -34,9 +37,9 @@ class TeaForm(forms.ModelForm):
         }
         fields = [
             'name',
-            'store',
             'price_per_100_grams',
             'grams_left',
+            'store',
             'country',
             'province',
             'region',
@@ -56,3 +59,25 @@ class TeaForm(forms.ModelForm):
                 attrs={'type': 'date'},
             ),
         }
+
+    def clean(self) -> dict[str, Any]:
+        cleaned_data = super(TeaForm, self).clean()
+
+        harvest_year = (
+            cleaned_data.get('year') if cleaned_data.get('year') is not None else 1
+        )
+        if harvest_year > date.today().year:
+            self.add_error('year', 'Rok produkcji jest większy niż aktualny rok!')
+
+        harvest_date = (
+            cleaned_data.get('harvest_date')
+            if cleaned_data.get('harvest_date') is not None
+            else date(1, 1, 1)
+        )
+        if harvest_date > date.today():
+            self.add_error('harvest_date', 'Data zbiorów jest w przyszłości!')
+
+        if harvest_year != harvest_date.year:
+            self.add_error('year', 'Rok nie zgadza się z datą zbiorów!')
+
+        return cleaned_data
