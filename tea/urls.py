1b0f83f59cc1e6d9f5fe196cd84399f06f7a6c61
Łukasz Wójcik <lukasz.wojcik.22@gmail.com>
2024-02-10 23:04:26 +0100

    Change creation url from nowa to dodaj

--- tea/urls.py
+++ tea/urls.py
@@ -7,6 +7,6 @@ from . import views
 urlpatterns = [
     path('', views.MainPageView.as_view(), name='main-page'),
     path('herbaty', views.AllTeasView.as_view(), name='all-teas'),
-    path('herbaty/nowa', views.TeaCreateView.as_view(), name='create-tea'),
+    path('herbaty/dodaj', views.TeaCreateView.as_view(), name='create-tea'),
     path('herbaty/<slug:slug>', views.TeaDetailView.as_view(), name='tea-detail'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
