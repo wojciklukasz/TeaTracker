fe5e21ed3f62690e4b0de3da46344313b094f739
Łukasz Wójcik <lukasz.wojcik.22@gmail.com>
2024-02-11 15:52:59 +0100

    Add tests for dates validation and creating other models, change static addresses to reverses

--- tea/tests/test_create_tea.py
+++ tea/tests/test_create_tea.py
@@ -1,24 +1,25 @@
 from http import HTTPStatus
 
 from django.test import TestCase
+from django.urls import reverse_lazy
 
-from tea.models import Profile
+from tea import models
 
 
 class CreateTeaTestCase(TestCase):
     @classmethod
     def setUpTestData(cls) -> None:
-        Profile.objects.create(name='default')
+        models.Profile.objects.create(name='default')
 
     def test_page_load(self):
-        response = self.client.get('/herbaty/nowa')
+        response = self.client.get(reverse_lazy('create-tea'))
 
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertContains(response, '<h1>Nowa herbata</h1>', html=True)
 
     def test_incorrect_form(self):
         response = self.client.post(
-            '/herbaty/nowa',
+            reverse_lazy('create-tea'),
             data={
                 'name': '',
                 'price_per_100_grams': '1',
@@ -31,7 +32,7 @@ class CreateTeaTestCase(TestCase):
 
     def test_valid_form(self):
         response = self.client.post(
-            '/herbaty/nowa',
+            reverse_lazy('create-tea'),
             data={
                 'name': 'Test Tea',
                 'price_per_100_grams': '1',
@@ -41,14 +42,14 @@ class CreateTeaTestCase(TestCase):
 
         self.assertRedirects(
             response,
-            '/herbaty/test-tea',
+            reverse_lazy('tea-detail', kwargs={'slug': 'test-tea'}),
             status_code=HTTPStatus.FOUND,
             target_status_code=HTTPStatus.OK,
         )
 
     def test_slug_creation(self):
         self.client.post(
-            '/herbaty/nowa',
+            reverse_lazy('create-tea'),
             data={
                 'name': 'Test Tea',
                 'price_per_100_grams': '1',
@@ -57,7 +58,7 @@ class CreateTeaTestCase(TestCase):
         )
 
         response = self.client.post(
-            '/herbaty/nowa',
+            reverse_lazy('create-tea'),
             data={
                 'name': 'Test Tea',
                 'price_per_100_grams': '1',
@@ -67,7 +68,88 @@ class CreateTeaTestCase(TestCase):
 
         self.assertRedirects(
             response,
-            '/herbaty/test-tea-1',
+            reverse_lazy('tea-detail', kwargs={'slug': 'test-tea-1'}),
             status_code=HTTPStatus.FOUND,
             target_status_code=HTTPStatus.OK,
         )
+
+    def test_unequal_dates(self):
+        response = self.client.post(
+            reverse_lazy('create-tea'),
+            data={
+                'name': 'Test Tea',
+                'price_per_100_grams': '1',
+                'grams_left': '1',
+                'harvest_date': '2022-01-01',
+                'year': '2023',
+            },
+        )
+
+        # print(response.content)
+        # self.assertContains(response, '<ul class="errorlist">', html=True)
+        self.assertContains(response, 'Rok nie zgadza się z datą zbiorów!')
+
+    def test_future_dates(self):
+        response = self.client.post(
+            reverse_lazy('create-tea'),
+            data={
+                'name': 'Test Tea',
+                'price_per_100_grams': '1',
+                'grams_left': '1',
+                'harvest_date': '2025-01-01',
+                'year': '2025',
+            },
+        )
+
+        # print(response.content)
+        # self.assertContains(response, '<ul class="errorlist">', html=True)
+        self.assertContains(response, 'Rok produkcji jest większy niż aktualny rok!')
+        self.assertContains(response, 'Data zbiorów jest w przyszłości!')
+
+    def test_plus_buttons(self):
+        response = self.client.get(reverse_lazy('create-tea'))
+
+        self.assertContains(
+            response,
+            '<img class="plus-icon" src="/static/images/plus_icon.svg" alt="Biały plus z zielonym tłem">',
+            6,
+            html=True,
+        )
+
+    def test_create_cultivar(self):
+        response = self.client.post(
+            reverse_lazy('create-cultivar'),
+            data={
+                'name': 'Test Cultivar',
+            },
+        )
+
+        self.assertRedirects(
+            response,
+            reverse_lazy('create-tea'),
+            status_code=HTTPStatus.FOUND,
+            target_status_code=HTTPStatus.OK,
+        )
+
+        response = self.client.post(
+            reverse_lazy('create-tea'),
+            data={
+                'name': 'Test Tea',
+                'price_per_100_grams': '1',
+                'grams_left': '1',
+                'cultivar': models.Cultivar.objects.get(name='Test Cultivar').id,
+            },
+        )
+
+        self.assertRedirects(
+            response,
+            reverse_lazy('tea-detail', kwargs={'slug': 'test-tea'}),
+            status_code=HTTPStatus.FOUND,
+            target_status_code=HTTPStatus.OK,
+        )
+
+        response = self.client.get(
+            reverse_lazy('tea-detail', kwargs={'slug': 'test-tea'})
+        )
+
+        self.assertContains(response, 'Test Cultivar')
