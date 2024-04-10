31cc593d38f7c6ab54ad38fa65ca77362e17a2bc
Łukasz Wójcik <lukasz.wojcik.22@gmail.com>
2024-04-10 14:02:52 +0200

    Change verbose name of Profile to Polish, reformat code

--- tea/models.py
+++ tea/models.py
@@ -7,29 +7,32 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Profile(models.Model):
-    name = models.CharField(max_length=50, unique=True, verbose_name=_('Nazwa'))
+    name = models.CharField(max_length=50, unique=True, verbose_name=_("Nazwa"))
 
     def __str__(self) -> str:
         return self.name
 
+    class Meta:
+        verbose_name = _("Profil")
+
 
 class Store(models.Model):
-    name = models.CharField(max_length=50, unique=True, verbose_name=_('Nazwa'))
-    website = models.URLField(unique=True, verbose_name=_('Strona internetowa'))
+    name = models.CharField(max_length=50, unique=True, verbose_name=_("Nazwa"))
+    website = models.URLField(unique=True, verbose_name=_("Strona internetowa"))
 
     def __str__(self) -> str:
         return self.name
 
 
 class Type(models.Model):
-    name = models.CharField(max_length=25, unique=True, verbose_name=_('Nazwa'))
+    name = models.CharField(max_length=25, unique=True, verbose_name=_("Nazwa"))
 
     def __str__(self) -> str:
         return self.name
 
 
 class Cultivar(models.Model):
-    name = models.CharField(max_length=50, unique=True, verbose_name=_('Nazwa'))
+    name = models.CharField(max_length=50, unique=True, verbose_name=_("Nazwa"))
 
     def __str__(self) -> str:
         return self.name
@@ -37,18 +40,18 @@ class Cultivar(models.Model):
 
 class Country(models.Model):
     class Meta:
-        verbose_name_plural = 'Countries'
+        verbose_name_plural = "Countries"
 
-    name = models.CharField(max_length=25, unique=True, verbose_name=_('Nazwa'))
+    name = models.CharField(max_length=25, unique=True, verbose_name=_("Nazwa"))
 
     def __str__(self) -> str:
         return self.name
 
 
 class Province(models.Model):
-    name = models.CharField(max_length=30, unique=True, verbose_name=_('Nazwa'))
+    name = models.CharField(max_length=30, unique=True, verbose_name=_("Nazwa"))
     country = models.ForeignKey(
-        Country, on_delete=models.CASCADE, verbose_name=_('Kraj')
+        Country, on_delete=models.CASCADE, verbose_name=_("Kraj")
     )
 
     def __str__(self) -> str:
@@ -56,9 +59,9 @@ class Province(models.Model):
 
 
 class Region(models.Model):
-    name = models.CharField(max_length=30, unique=True, verbose_name=_('Nazwa'))
+    name = models.CharField(max_length=30, unique=True, verbose_name=_("Nazwa"))
     province = models.ForeignKey(
-        Province, on_delete=models.CASCADE, verbose_name=_('Prowincja')
+        Province, on_delete=models.CASCADE, verbose_name=_("Prowincja")
     )
 
     def __str__(self) -> str:
@@ -66,59 +69,59 @@ class Region(models.Model):
 
 
 class Tea(models.Model):
-    SEASON_CHOICES = [('W', 'Wiosna'), ('L', 'Lato'), ('J', 'Jesień'), ('Z', 'Zima')]
+    SEASON_CHOICES = [("W", "Wiosna"), ("L", "Lato"), ("J", "Jesień"), ("Z", "Zima")]
 
     profile = models.ForeignKey(
-        Profile, on_delete=models.CASCADE, verbose_name=_('Profil')
+        Profile, on_delete=models.CASCADE, verbose_name=_("Profil")
     )
-    name = models.CharField(max_length=100, verbose_name=_('Nazwa'))
+    name = models.CharField(max_length=100, verbose_name=_("Nazwa"))
     slug = models.SlugField(unique=True)
     store = models.ForeignKey(
-        Store, null=True, blank=True, on_delete=models.SET_NULL, verbose_name=_('Sklep')
+        Store, null=True, blank=True, on_delete=models.SET_NULL, verbose_name=_("Sklep")
     )
 
     price_per_100_grams = models.FloatField(
-        validators=[MinValueValidator(1)], verbose_name=_('Cena za 100 gramów')
+        validators=[MinValueValidator(1)], verbose_name=_("Cena za 100 gramów")
     )
     grams_left = models.IntegerField(
-        validators=[MinValueValidator(0)], verbose_name=_('Pozostało gramów')
+        validators=[MinValueValidator(0)], verbose_name=_("Pozostało gramów")
     )
     score = models.IntegerField(
         null=True,
         blank=True,
         validators=[MinValueValidator(1), MaxValueValidator(10)],
-        verbose_name=_('Ocena'),
+        verbose_name=_("Ocena"),
     )
 
     date_added = models.DateTimeField(auto_now_add=True)
     last_viewed = models.DateTimeField(default=timezone.now, blank=True)
     date_finished = models.DateField(null=True, blank=True)
 
-    store_description = models.TextField(blank=True, verbose_name=_('Opis sprzedawcy'))
-    user_description = models.TextField(blank=True, verbose_name=_('Opis własny'))
-    tasting_notes = models.TextField(blank=True, verbose_name=_('Nuty smakowe'))
-    additional_notes = models.TextField(blank=True, verbose_name=_('Dodatkowe uwagi'))
+    store_description = models.TextField(blank=True, verbose_name=_("Opis sprzedawcy"))
+    user_description = models.TextField(blank=True, verbose_name=_("Opis własny"))
+    tasting_notes = models.TextField(blank=True, verbose_name=_("Nuty smakowe"))
+    additional_notes = models.TextField(blank=True, verbose_name=_("Dodatkowe uwagi"))
 
     country = models.ForeignKey(
         Country,
         null=True,
         blank=True,
         on_delete=models.SET_NULL,
-        verbose_name=_('Kraj'),
+        verbose_name=_("Kraj"),
     )
     province = models.ForeignKey(
         Province,
         null=True,
         blank=True,
         on_delete=models.SET_NULL,
-        verbose_name=_('Prowincja'),
+        verbose_name=_("Prowincja"),
     )
     region = models.ForeignKey(
         Region,
         null=True,
         blank=True,
         on_delete=models.SET_NULL,
-        verbose_name=_('Region'),
+        verbose_name=_("Region"),
     )
 
     type = models.ForeignKey(
@@ -126,22 +129,22 @@ class Tea(models.Model):
         null=True,
         blank=True,
         on_delete=models.SET_NULL,
-        verbose_name=_('Typ herbaty'),
+        verbose_name=_("Typ herbaty"),
     )
     cultivar = models.ForeignKey(
         Cultivar,
         null=True,
         blank=True,
         on_delete=models.SET_NULL,
-        verbose_name=_('Kultywar'),
+        verbose_name=_("Kultywar"),
     )
 
     season = models.CharField(
-        blank=True, max_length=1, choices=SEASON_CHOICES, verbose_name=_('Sezon')
+        blank=True, max_length=1, choices=SEASON_CHOICES, verbose_name=_("Sezon")
     )
-    year = models.IntegerField(null=True, blank=True, verbose_name=_('Rok'))
+    year = models.IntegerField(null=True, blank=True, verbose_name=_("Rok"))
     harvest_date = models.DateField(
-        null=True, blank=True, verbose_name=_('Data zbiorów')
+        null=True, blank=True, verbose_name=_("Data zbiorów")
     )
 
     def __str__(self) -> str:
@@ -150,10 +153,10 @@ class Tea(models.Model):
 
 class Image(models.Model):
     tea = models.ForeignKey(Tea, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='tea-images')
+    image = models.ImageField(upload_to="tea-images")
 
     def __str__(self) -> str:
-        return f'{self.id} - {self.tea.name}'
+        return f"{self.id} - {self.tea.name}"
 
 
 class Brew(models.Model):
@@ -162,12 +165,12 @@ class Brew(models.Model):
     tasting_notes = models.TextField()
 
     def __str__(self) -> str:
-        return f'{self.tea.name} {self.brew_date}'
+        return f"{self.tea.name} {self.brew_date}"
 
 
 class BrewImage(models.Model):
     brew = models.ForeignKey(Brew, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='brew-images')
+    image = models.ImageField(upload_to="brew-images")
 
     def __str__(self) -> str:
-        return f'{self.id} - {self.brew.brew_date} - {self.brew.tea}'
+        return f"{self.id} - {self.brew.brew_date} - {self.brew.tea}"
