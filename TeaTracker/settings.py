67ff3f62caa79de070f8cc58b81c8b242fbfbba6
Łukasz Wójcik <lukasz.wojcik.22@gmail.com>
2024-04-12 16:36:00 +0200

    Change default allowed hosts to *

--- TeaTracker/settings.py
+++ TeaTracker/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.getenv("IS_DEV", "True") == "True" else False
 
-ALLOWED_HOSTS = [os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1")]
+ALLOWED_HOSTS = [os.getenv("DJANGO_ALLOWED_HOSTS", "*")]
 
 
 # Application definition
