af33772549bec9179275083c14c2c9f012a36cd2
Łukasz Wójcik <lukasz.wojcik.22@gmail.com>
2024-04-16 22:42:56 +0200

    Get the current tea when creating a new brew

--- tea/views.py
+++ tea/views.py
@@ -296,3 +296,11 @@ class BrewCreateView(CreateView):
     def get_success_url(self) -> str:
         print(self.object.slug)
         return reverse_lazy("tea-detail", kwargs={"slug": self.object.slug})
+
+    def form_valid(self, form: forms.BrewForm) -> HttpResponse:
+        tea_slug = self.kwargs["slug"]
+        tea = models.Tea.objects.get(slug=tea_slug)
+
+        form.instance.tea = tea
+
+        return super().form_valid(form)
